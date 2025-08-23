ffe57982d2599cc4abdf99b3519c5a2fa19bbc17
Soulemane Sow <soulemane@lillup.com>
2025-08-23 16:19:47 -0400

    Make logo much bigger in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,9 +46,9 @@ export function Header({ currentPage = 'home' }: HeaderProps) {
           <Image 
             src="/logo.png" 
             alt="WellNex02 Logo" 
-            width={150} 
-            height={150} 
-            className="h-36 w-36"
+            width={200} 
+            height={200} 
+            className="h-48 w-48"
             priority
           />
         </Link>
