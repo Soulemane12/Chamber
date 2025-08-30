752e348a2c197451b728a0d3b63653212c0b9863
Soulemane Sow <soulemane@lillup.com>
2025-08-29 22:28:37 -0400

    Increase video width to 500px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,14 +56,14 @@ export default function Home() {
                 {t('readMore')}
               </Link>
               <div className="mt-6 flex justify-center">
-                <div className="max-w-[400px] mx-auto">
+                <div className="max-w-[500px] mx-auto">
                   <video 
                     src="/Wellnex_vid.MOV" 
                     controls 
                     className="w-full rounded-lg shadow-lg"
-                    width="400"
+                    width="500"
                     height="auto"
-                    style={{ maxWidth: '400px', margin: '0 auto' }}
+                    style={{ maxWidth: '500px', margin: '0 auto' }}
                   >
                     Your browser does not support the video tag.
                   </video>
